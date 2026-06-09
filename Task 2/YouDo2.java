be20ba8930eb20d60d743e9d386ac4675d5f10fe
Chaity215 <jannatchaity215@gmail.com>
2026-06-08 21:08:08 -0800

    Update YouDo2.java

--- Task 2/YouDo2.java
+++ Task 2/YouDo2.java
@@ -1,1 +1,11 @@
-
+class NumberChecker {
+    void check (int num) {
+        System.out.println((num % 2 == 0) && (num > 10));
+    }
+}
+public class Main {
+    public static void  main(String[] args) {
+        NumberChecker nc = new NumberChecker();
+        nc.check(14);
+    }
+}
