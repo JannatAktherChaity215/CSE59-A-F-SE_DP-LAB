c8423d4e14a6dc1396800d815885e79ada8ecd75
Chaity215 <jannatchaity215@gmail.com>
2026-06-08 21:09:48 -0800

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,16 @@
-
+class Painter {
+     void printData(String text) {
+         System.out.println(text);
+     }
+      void printData(int number) {
+         System.out.println(number);
+     }
+}
+public class Main {
+    public static void main(String[] args) {
+        Painter p = new Painter();
+        
+        p.printData("Hello Java");
+        p.printData(100);
+    }
+}
