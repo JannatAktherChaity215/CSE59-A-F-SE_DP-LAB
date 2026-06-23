69a4b84acd4bf027ac264b0cf0df033870604154
Chaity215 <jannatchaity215@gmail.com>
2026-06-22 21:30:47 -0800

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,55 @@
 
+// Abstraction
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+
+// Low-level class (Email)
+class EmailSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+// Low-level class (SMS)
+class SMSSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+
+// High-level module
+class NotificationService {
+
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+
+
+// MAIN CLASS
+public class Main {
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+        NotificationService service1 = new NotificationService(email);
+        service1.alertUser("Hello via Email");
+
+        System.out.println("----");
+
+        MessageSender sms = new SMSSender();
+        NotificationService service2 = new NotificationService(sms);
+        service2.alertUser("Hello via SMS");
+    }
+}
