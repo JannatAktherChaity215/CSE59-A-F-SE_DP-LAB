adcb1cd1d2fcefc416c4e4bf77c447f0155543b2
Chaity215 <jannatchaity215@gmail.com>
2026-06-22 21:20:51 -0800

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,59 @@
 
+// Common parent
+class TeamMember {
+    protected String name;
+    public TeamMember(String name) {
+        this.name = name;
+    }
+    public void showName() {
+        System.out.println(name);
+        
+    }
+}
+// Only bonus eligible people
+interface BonusEligible {
+    void calculateBonus();
+}
+// Regular Employee
+class Employee extends TeamMember
+implements BonusEligible {
+    public Employee(String name){
+        super(name);
+    }
+    
+
+public void calculateBonus() {
+    System.out.println(
+        "Calculating employee bonus..."
+        );
+}
+}
+// Contractor is not bonus eligible
+class Contractor extends TeamMember {
+    public Contractor(String name){
+        super(name);
+    }
+
+
+public void work() {
+    System.out.println(
+        "contractor working..."
+        );
+}
+}
+
+// Test
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = 
+        new Employee("Rahim");
+        
+         Contractor con = 
+         new Contractor("Karim");
+         
+         emp.calculateBonus();
+         
+         con.work();
+    }
+}
+   
