4bf4492f46d024edcefc65b448f3d8b8b37fd0a0
Chaity215 <jannatchaity215@gmail.com>
2026-06-29 21:36:48 -0800

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,33 @@
+interface OptimizableModel{
+    void quantize();
 
+}
+class ResNet18 implements OptimizableModel{
+    public void quantize(){
+
+System.out.println("ResNet18");
+
+}
+
+}
+
+class MobileNet implements OptimizableModel{
+    public void quantize(){
+        System.out.println("MobileNet");
+    }
+}
+class InferenceEngine{
+    void optimize(OptimizableModel model){
+        model.quantize();
+
+    }
+}
+public class Main4{
+    public static void main(String[] args){
+
+InferenceEngine e =
+new InferenceEngine();
+e.optimize(new ResNet18());
+e.optimize(new MobileNet());
+}
+}
