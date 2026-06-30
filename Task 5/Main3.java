bb75e698f97fc4c5702043a5a452b085174b410d
Chaity215 <jannatchaity215@gmail.com>
2026-06-29 21:36:22 -0800

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,41 @@
 
+class Patient{
+    String name;
+    String id;
+    Patient(String name,String id){
+        this.name=name;
+        this.id=id;
+    }
+}
+class IdValidator{
+    boolean validator(String id){
+        return id.length()==10 || id.length()==17;
+    }
+
+    }
+    class SmsService{
+        void  sendSMS(Patient p){
+            System.out.println(
+            "SMS sent to "+p.name
+        );
+
+        }
+    }
+    public class Main3{
+        public static void main(String[] args){
+
+
+Patient p =
+new Patient("John","1234567890");
+
+IdValidator v =
+new IdValidator();
+
+if (v.validator(p.id)){
+
+    SmsService sms =
+   new SmsService();
+    sms.sendSMS(p);
+}
+    }
+}
