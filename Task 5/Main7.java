3ceec5da0e8a9646dd9c4542d071c2fc80660186
Chaity215 <jannatchaity215@gmail.com>
2026-06-29 21:38:21 -0800

    Update Main7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,61 @@
 
+interface INetworkProtocol{
+
+    void broadcastMessage(String msg);
+
+}
+
+class BluetoothMeshNetwork
+implements INetworkProtocol{
+
+
+    public void broadcastMessage(String msg){
+
+        System.out.println(
+        "Bluetooth " + msg
+        );
+
+    }
+
+}
+
+class DisasterAlertSystem{
+
+
+    INetworkProtocol network;
+
+
+    DisasterAlertSystem(
+    INetworkProtocol network){
+
+        this.network = network;
+
+    }
+
+
+    void triggerAlert(String msg){
+
+        network.broadcastMessage(msg);
+
+    }
+
+}
+
+public class Main7{
+
+
+    public static void main(String[] args){
+
+
+        DisasterAlertSystem d =
+        new DisasterAlertSystem(
+        new BluetoothMeshNetwork()
+        );
+
+
+        d.triggerAlert("Emergency");
+
+
+    }
+
+}
