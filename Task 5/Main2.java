df809288df29d02eb7d453ff8e5adf4b6c90c4e7
Chaity215 <jannatchaity215@gmail.com>
2026-06-29 21:35:34 -0800

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,43 @@
 
+interface MessageSender{
+    void sendMessage(String msg);
+}
+class EmailSender implements MessageSender{
+    public void sendMessage(String msg){
+    System.out.println("Email: "+msg);
+
+}
+}
+
+class SMSender implements MessageSender{
+
+    public void sendMessage(String msg) {
+        System.out.println("SMS : "+msg);
+
+        
+    }
+}
+
+class NotificationService{
+
+MessageSender sender;
+NotificationService(MessageSender sender){
+    this.sender=sender;
+
+}
+void alertUser(String msg){
+    sender.sendMessage(msg);
+}
+}
+public class Main2{
+    public static void main(String[] args){
+        NotificationService n = 
+        new NotificationService(new EmailSender());
+
+    n.alertUser("Welcome");
+NotificationService s =
+        new NotificationService(new SMSender());
+        s.alertUser("OTP");
+
+    }
+}
