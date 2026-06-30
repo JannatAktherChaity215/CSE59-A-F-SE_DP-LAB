cddf1ce57a8e29690faf4c89a7106709bda4168e
Chaity215 <jannatchaity215@gmail.com>
2026-06-29 21:37:48 -0800

    Update Main6.java

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,66 @@
 
+interface HttpService{
+
+    void handleHttpRequest();
+
+}
+
+
+interface DatabaseManager{
+
+    void executeSqlStatement();
+
+}
+
+
+
+class WebController implements HttpService{
+
+
+    public void handleHttpRequest(){
+
+        System.out.println("HTTP");
+
+    }
+
+}
+
+
+
+class DatabaseServer implements DatabaseManager{
+
+
+    public void executeSqlStatement(){
+
+        System.out.println("SQL");
+
+    }
+
+}
+
+
+
+public class Main6{
+
+
+    public static void main(String[] args){
+
+
+        WebController w =
+        new WebController();
+
+
+        w.handleHttpRequest();
+
+
+
+        DatabaseServer d =
+        new DatabaseServer();
+
+
+        d.executeSqlStatement();
+
+
+    }
+
+}
