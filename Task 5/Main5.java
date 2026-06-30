bac0f66e2229259f1a91a33bee7bf5ab85d8fd4e
Chaity215 <jannatchaity215@gmail.com>
2026-06-29 21:37:11 -0800

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,31 @@
 
+class UniversityMember{
+    void attendLecture(){
+        System.out.println("Lecture");
+
+        }
+    }
+    interface Researcher{
+        void conductLabResearch();
+    }
+    class Professor extends UniversityMember
+    implements Researcher{
+        public void conductLabResearch(){
+       System.out.println("Research");
+    }
+    }
+    class UndergraduateStudent
+    extends UniversityMember{
+
+    }
+public class Main5{
+    public static void main(String[] args){
+       UndergraduateStudent u =
+       new UndergraduateStudent();
+       u.attendLecture();
+       Professor p=
+       new Professor();
+       p.conductLabResearch();
+
+    }
+}
